e09aaba9a64206767ed61f894b53b0b237450140
BenDiekhoff <ben.diekhoff@gmail.com>
2020-11-14 22:30:48 -0600

    tried to make gdal work

--- Assignments/A05/nxgraph.py
+++ Assignments/A05/nxgraph.py
@@ -1,10 +1,13 @@
 
+import os
+os.environ['PROJ_LIB'] = 'C:\\Users\\Ben-Study\\anaconda3\\envs\\maps2\\Library\\include\\boost\\geometry\\srs\\projections\\proj'
+os.environ['GDAL_DATA'] = 'C:\\Users\\Ben-Study\\Desktop\\gdal stuff\\GDAL-3.1.3\\osgeo\\data\\proj'
 import networkx as nx
 import matplotlib.pyplot as plt
 
 # PR = "Assignments\\A05\\Data\\Primary_Roads.geojson"
 RR = "Assignments\\A05\\Data\\railroads.shp"
 
-railGraph = nx.read_shp(RR, simplify=False, geom_attrs=True,strict=True)
-
+railGraph = nx.read_shp(RR)#, simplify=False, geom_attrs=True,strict=True)
+print("helo")
 
