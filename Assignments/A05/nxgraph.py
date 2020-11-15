f1163812d875b336cc074a11ce88e7da53539319
BenDiekhoff <ben.diekhoff@gmail.com>
2020-11-15 03:51:16 -0600

    finds intersections

--- Assignments/A05/nxgraph.py
+++ Assignments/A05/nxgraph.py
@@ -1,13 +1,53 @@
 
 import os
-os.environ['PROJ_LIB'] = 'C:\\Users\\Ben-Study\\anaconda3\\envs\\maps2\\Library\\include\\boost\\geometry\\srs\\projections\\proj'
-os.environ['GDAL_DATA'] = 'C:\\Users\\Ben-Study\\Desktop\\gdal stuff\\GDAL-3.1.3\\osgeo\\data\\proj'
 import networkx as nx
 import matplotlib.pyplot as plt
+import gdal
+from shapely.geometry import Point, shape
+import fiona
+import itertools
+
 
 # PR = "Assignments\\A05\\Data\\Primary_Roads.geojson"
-RR = "Assignments\\A05\\Data\\railroads.shp"
+RR = "Assignments/A05/Data/railroadsSUB.shp"
+lines = [shape(line['geometry']) for line in fiona.open(RR)]
+endpts = [(Point(list(line.coords)[0]), Point(list(line.coords)[-1])) for line  in lines]
+# flatten the resulting list to a simple list of points
+endpts= [pt for sublist in endpts for pt in sublist] 
+# print(endpts)
 
-railGraph = nx.read_shp(RR)#, simplify=False, geom_attrs=True,strict=True)
+
+inters = []
+i = 0
+for line1,line2 in  itertools.combinations(lines, 2):
+  if  line1.intersects(line2):
+    inter = line1.intersection(line2)
+    if "Point" == inter.type:
+        # print("1")
+        inters.append(inter)
+    elif "MultiPoint" == inter.type:
+        print("2")
+        inters.extend([pt for pt in inter])
+  
+
+# result = endpts.extend([pt for pt in inters  if pt not in endpts])
+# for pt in inters:
+#     print(pt)
+
+from shapely.geometry import mapping
+# schema of the shapefile
+schema = {'geometry': 'Point','properties': {'test': 'int'}}
+# creation of the shapefile
+with fiona.open('result.shp','w','ESRI Shapefile', schema) as output:
+    for i, pt in enumerate(inters):
+        output.write({'geometry':mapping(pt), 'properties':{'test':i}})
+
+# railGraph = nx.read_shp(RR, simplify=False, geom_attrs=True, strict=True)
+
+# nx.draw(railGraph, with_labels=True)
+# plt.draw()
+# plt.show()
+# , simplify=False, geom_attrs=True,strict=True)
+# plt.savefig("Assignments/A05/Data/rr")
 print("helo")
 
