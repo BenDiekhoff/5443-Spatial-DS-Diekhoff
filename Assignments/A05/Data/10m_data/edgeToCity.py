00ae848a4711a73837a7eb17f2388f2ae29b1594
BenDiekhoff <ben.diekhoff@gmail.com>
2020-11-22 17:11:19 -0600

    edges

--- Assignments/A05/Data/10m_data/edgeToCity.py
+++ Assignments/A05/Data/10m_data/edgeToCity.py
@@ -17,14 +17,17 @@ ROADS = "Assignments/A05/Data/10m_data/roads.geojson"
 INPUT_SHAPEFILE ="Assignments/A05/Data/10m_data/ne_10m_roads_north_america.shp"
 OUTPUT = 'Assignments/A05/Data/10m_data/roads'
 
+print("loading map")
 G = nx.read_shp(INPUT_SHAPEFILE, simplify=True)
-print("map read in")
+
+print("converting map")
 G = G.to_undirected()
-print("map converted")
+
 
 print("reading cities")
 with open(CITIES, "r") as f:
     cities = json.load(f)
+    
 print("making nodelist")
 nodelist=[]
 for feature in cities["features"]:
@@ -42,10 +45,14 @@ for feature in roads["features"]:
     edgelist.append(edge)
 
 print("beginning haversine")
+i = 0
+max = len(nodelist)
 for node in nodelist:
     minDist = 5
     closestEdge =(0,0)
     found = False
+    i+=1
+    print(f"calculating node {i} of {max}")
     for edge in edgelist:
         dist = haversine(node, edge, unit = "mi")
         if dist < minDist:
