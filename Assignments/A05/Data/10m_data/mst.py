30daf0647ed90d50c99a3aac86fbc95d65fa5360
BenDiekhoff <ben.diekhoff@gmail.com>
2020-12-02 17:58:40 -0600

    MST

--- Assignments/A05/Data/10m_data/mst.py
+++ Assignments/A05/Data/10m_data/mst.py
@@ -14,11 +14,12 @@ import shapefile
 CITIES = "Assignments/A05/Data/10m_data/cities.geojson"
 ROADS = "Assignments/A05/Data/10m_data/roads.geojson"
 INPUT_SHAPEFILE ="Assignments/A05/Data/10m_data/roads/edges.shp"
-OUTPUT = 'Assignments/A05/Data/10m_data/roads'
-# OUTPUT = 'Assignments/A05/Data/10m_data/roads2'
+# OUTPUT = 'Assignments/A05/Data/10m_data/mst'
+OUTPUT = 'Assignments/A05/Data/10m_data/mst2'
+
 
 print("loading map")
-if OUTPUT =='Assignments/A05/Data/10m_data/roads':
+if OUTPUT =='Assignments/A05/Data/10m_data/mst':
     G = nx.read_shp(INPUT_SHAPEFILE, simplify=True)
 else:
     G = nx.read_shp(INPUT_SHAPEFILE, simplify=False)
@@ -26,6 +27,25 @@ else:
 print("converting map")
 G = G.to_undirected()
 
+
+# T = nx.minimum_spanning_tree(G,weight=str(G.edges(data=True)[2]['length']))
+# nx.write_shp(T, OUTPUT)
+
+
+# H = nx.Graph()
+# H = H.to_undirected()
+
+# for edge in G.edges(data=True):
+#     # input(edge)
+#     dist = edge[2]['length']
+#     # input(dist)
+#     # input(type(dist))
+#     H.add_edge(edge[0], edge[1],weight = dist)
+
+# # T = nx.minimum_spanning_tree(H)
+# nx.write_shp(H,OUTPUT)
+
+###############################################################################
 print("reading cities")
 with open(CITIES, "r") as f:
     cities = json.load(f)
@@ -36,35 +56,82 @@ for feature in cities["features"]:
     coords = (feature["geometry"]["coordinates"][0] ,feature["geometry"]["coordinates"][1])
     nodelist.append(coords)
 
-print("reading roads")
-with open (ROADS, "r") as f:
-    roads = json.load(f)
+# input(len(nodelist))
 
-print("making edgelist")
-edgelist=[]
-for feature in roads["features"]:
-    for coord in feature['geometry']['coordinates'][0]:
-        edgelist.append((coord[0], coord[1]))
+# print("reading roads")
+# with open (ROADS, "r") as f:
+#     roads = json.load(f)
 
+# print("making edgelist")
+# edgelist=[]
+# for feature in roads["features"]:
+#     for coord in feature['geometry']['coordinates'][0]:
+#         edgelist.append((coord[0], coord[1]))
+
+
+path =[]
 i = 0
 max = len(nodelist)
+print("calculating paths")
 for node in nodelist:
     i+=1
     print(f"calculating paths for node {i} of {max}")
     for endnode in nodelist:
         try:
-            input(nx.shortest_path(G, source=tuple(node), target=tuple(endnode)))
-            print(f"node: {node}, endnode: {endnode}")
-
-            # input(path)
+            # input(f"node: {node}, endnode: {endnode}")
+            coords = nx.shortest_path(G, source=tuple(node), target=tuple(endnode))
+            if len(coords) >1:
+                path.append(coords)
+            # pathSet= set(tuple(i)for i in path)
+            # input(pathSet)
+        
         except:
-
             pass
-        # path = nx.shortest_path(G,source=tuple(x), target=tuple(y),weight=None,method="dijkstra")
+
+pathSet= set(tuple(i)for i in path)
+print(len(pathSet))
+
+
+pathJson = {
+    "type": "FeatureCollection",
+    "features": []
+    }
+
+pathdict = {
+    'type':'Feature',
+    "properties":{},
+    'geometry':{
+        'type': "MultiLineString", 
+        "coordinates":[
+
+        ]
+    }
+}
+
+for path in pathSet:
+    for coords in path:
+        coords=list(coords)
+        # input(coords)
+    path=list(path)
+    # input(path)
+    # pathdict = {
+    #     'type':'Feature',
+    #     "properties":{},
+    #     'geometry':{
+    #         'type': "MultiLineString", 
+    #         "coordinates":[
+
+    #         ]
+    #     }
+    # }
+    pathdict['geometry']['coordinates'].append(path)
+
+    pathJson['features'].append(pathdict)
+
+
+
+with open (OUTPUT +'/paths.geojson','w+') as f:
+    data = json.dumps(pathJson)
+    f.write(data)
         
 
-        # try:
-        #     path = nx.shortest_path(G,node, endnode)
-        # except:
-        #     pass
-        # print(path)
